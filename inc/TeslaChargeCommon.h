9bfe095e9f1084214bf2913e310a8e7912008d05
Steve Bertrand <steveb@cpan.org>
2022-04-25 07:18:00 -0700

    Cleanup

--- inc/TeslaChargeCommon.h
+++ inc/TeslaChargeCommon.h
@@ -2,9 +2,9 @@
 #define TESLA_CHARGE_TESLACHARGE_COMMON_H
 
 #include <Arduino.h>
-#include <WiFiManager.h>
-#include <espnow.h>
 #include <ArduinoOTA.h>
+#include <espnow.h>
+#include <WiFiManager.h>
 
 #define SERIAL_DEBUG // Serial output (comment to disable)
 
@@ -21,7 +21,7 @@
 uint8_t MacInterface[] = {0x60, 0x01, 0x94, 0x71, 0xFF, 0xB0};
 uint8_t MacController[] = {0x5C, 0xCF, 0x7F, 0xCC, 0xA1, 0x8E};
 
-// Access Point name when the controllers are outside of a known wifi range
+// Access Point names when the controllers are outside a known wifi range
 
 const char* apNameInterface = "TeslaInterface";
 const char* apNameController = "TeslaController";
--- inc/TeslaChargeInterface.h
+++ inc/TeslaChargeInterface.h
@@ -1,8 +1,8 @@
 #ifndef TESLA_CHARGE_TESLACHARGE_INTERFACE_H
 #define TESLA_CHARGE_TESLACHARGE_INTERFACE_H
 
-#include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 #include "SSD1306Wire.h"
 #include "/Users/steve/repos/tesla-charge/inc/TeslaChargeFont.h"
@@ -12,22 +12,22 @@
 #define DEBUG_MAGNET    0   // REED switch on at all times
 #define DEBUG_MOTION    0   // PIR on at all times
 
-//#define URL             "http://192.168.1.252:55556"
+// The location of the backend API
 #define URL             "https://tesla.hellbent.app:55556";
 
-#define REED_PIN        2  // Pin D4
-#define PIR_PIN         12 // Pin D6
-#define ALARM_PIN       13 // Pin D7
-#define SDA_PIN         4  // Pin D2
-#define SCL_PIN         5  // Pin D1
+#define REED_PIN        2   // Pin D4
+#define PIR_PIN         12  // Pin D6
+#define ALARM_PIN       13  // Pin D7
+#define SDA_PIN         4   // Pin D2
+#define SCL_PIN         5   // Pin D1
 
 #define JSON_SIZE       224  // Maximum size of JSON data
 #define CHARGE_MAX      101  // Maximum battery level +1
 #define CHARGE_MIN      0    // Minimum battery level
-#define ALARM_ON_TIME   100  // Alarm cycle on time
-#define ALARM_OFF_TIME  3000 // Alarm cycle off time
-#define ALARM_CHARGE    70   // Below this, audible alarm sounds
+#define ALARM_ON_TIME   100  // Alarm cycle on time (milliseconds)
+#define ALARM_OFF_TIME  3000 // Alarm cycle off time (milliseconds)
+#define ALARM_CHARGE    70   // Below this battery level, audible alarm sounds
 
-#define DATA_DELAY      500 // ms to wait between API calls
+#define DATA_DELAY      500  // Wait between API calls (milliseconds)
 
 #endif
